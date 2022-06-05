bcc92b5e379ab246922efa19a988b5a6045951ef
guirguisnashatzaki <gogonashatzaki@gmail.com>
2022-06-05 10:59:08 +0200

    adding controllers with basic crud operations

---
+++ myproject/Controllers/CountriesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myproject.Data;
+using myproject.Models;
+
+namespace myproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly myprojectContext _context;
+
+        public CountriesController(myprojectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound();
+          }
+            return await _context.Countries.ToListAsync();
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(string id)
+        {
+          if (_context.Countries == null)
+          {
+              return NotFound();
+          }
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(string id, Country country)
+        {
+            if (id != country.Country1)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+          if (_context.Countries == null)
+          {
+              return Problem("Entity set 'myprojectContext.Countries'  is null.");
+          }
+            _context.Countries.Add(country);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CountryExists(country.Country1))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCountry", new { id = country.Country1 }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(string id)
+        {
+            if (_context.Countries == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(string id)
+        {
+            return (_context.Countries?.Any(e => e.Country1 == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ myproject/Controllers/PopulationsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myproject.Data;
+using myproject.Models;
+
+namespace myproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PopulationsController : ControllerBase
+    {
+        private readonly myprojectContext _context;
+
+        public PopulationsController(myprojectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Populations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Population>>> GetPopulations()
+        {
+          if (_context.Populations == null)
+          {
+              return NotFound();
+          }
+            return await _context.Populations.ToListAsync();
+        }
+
+        // GET: api/Populations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Population>> GetPopulation(int id)
+        {
+          if (_context.Populations == null)
+          {
+              return NotFound();
+          }
+            var population = await _context.Populations.FindAsync(id);
+
+            if (population == null)
+            {
+                return NotFound();
+            }
+
+            return population;
+        }
+
+        // PUT: api/Populations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPopulation(int id, Population population)
+        {
+            if (id != population.Myyear)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(population).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PopulationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Populations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Population>> PostPopulation(Population population)
+        {
+          if (_context.Populations == null)
+          {
+              return Problem("Entity set 'myprojectContext.Populations'  is null.");
+          }
+            _context.Populations.Add(population);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PopulationExists(population.Myyear))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPopulation", new { id = population.Myyear }, population);
+        }
+
+        // DELETE: api/Populations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePopulation(int id)
+        {
+            if (_context.Populations == null)
+            {
+                return NotFound();
+            }
+            var population = await _context.Populations.FindAsync(id);
+            if (population == null)
+            {
+                return NotFound();
+            }
+
+            _context.Populations.Remove(population);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PopulationExists(int id)
+        {
+            return (_context.Populations?.Any(e => e.Myyear == id)).GetValueOrDefault();
+        }
+    }
+}
