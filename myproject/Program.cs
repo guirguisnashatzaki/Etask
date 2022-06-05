5d196ca78e2d2311cb96373b53e9577c036633e4
guirguisnashatzaki <gogonashatzaki@gmail.com>
2022-06-05 10:54:43 +0200

    adding services

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using myproject.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<myprojectContext>(
+    opt =>
+    {
+        opt.UseMySql(
+            builder.Configuration.GetConnectionString("final"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.23-mysql")
+        );
+    }
+);
 
 var app = builder.Build();
 
